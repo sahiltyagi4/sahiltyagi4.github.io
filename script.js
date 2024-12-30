4039f13fba0ca517c3aae4f3547c1f7f3778b103
sahiltyagi4 <sahilt.tyagi@gmail.com>
2024-12-29 20:08:02 -0500

    light and dark mode

--- script.js
+++ script.js
@@ -4,19 +4,16 @@ const body = document.body;
 // Check local storage for the theme preference
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme === 'dark') {
-  body.classList.add('dark-mode');
-  toggleButton.textContent = 'Light Mode';
+    body.classList.add('dark-mode');
 }
 
 toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  
-  // Update button text based on the theme
-  if (body.classList.contains('dark-mode')) {
-    toggleButton.textContent = 'Light Mode';
-    localStorage.setItem('theme', 'dark');
-  } else {
-    toggleButton.textContent = 'Dark Mode';
-    localStorage.setItem('theme', 'light');
-  }
+    body.classList.toggle('dark-mode');
+    
+    // Save preference
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
 });
